715d5a95525c7f1712d3fa3fcd9a954baaa2b478
jakemeout <jacobosity@gmail.com>
2020-06-29 10:28:22 -0400

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,30 @@
+class Doctor
+    attr_accessor :name 
+
+    @@all = []
+    
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all 
+        @@all
+    end
+
+    def new_appointment(patient, date)
+        Appointment.new(date, patient,self)
+    end
+
+    def appointments
+        Appointment.all.select do |appts|
+            appts.doctor == self
+        end
+    end
+
+    def patients
+        appointments.map do |appts| 
+            appts.patient
+        end
+    end
+end
